5df2d631ef0e8562ed812e90c25d5419aa3b42f5
likun <genwocaigudian@gmail.com>
2020-09-10 14:40:21 +0800

    rbac

--- app/admin/controller/Test.php
+++ app/admin/controller/Test.php
@@ -12,7 +12,11 @@ class Test extends AdminBase
 //		Enforcer::addRoleForUser('eve', 'writer');
 //		Enforcer::addPolicy('writer', 'articles','edit');
 //		$res = Enforcer::getAllRoles();
-		$res = Enforcer::getPolicy();
-		return Show::success($res);
+//		$res = Enforcer::getAllRoles();
+//		$res = Enforcer::getPolicy();
+//        给用户分配角色
+        Enforcer::addRoleForUser('admin', 'admin');
+        Enforcer::addRoleForUser('admin1', 'member');
+        return Show::success($res);
     }
 }
