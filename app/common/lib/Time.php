d3396ac90e5fe40e8ff23bbe186810b3f51ed1af
likun <genwocaigudian@gmail.com>
2020-09-12 15:23:14 +0800

    repair

--- app/common/lib/Time.php
+++ app/common/lib/Time.php
@@ -10,6 +10,7 @@ class Time
         $type = !in_array($type, [1, 2]) ? 2 : $type;
         if ($type == 1) {
             $day = $type * 7;
+            $day = $type;
         } elseif ($type == 2) {
             $day = $type * 30;
         }
