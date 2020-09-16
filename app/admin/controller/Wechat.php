af3a49e7a8cc38cbac02c2cb85a1a0b034bad444
likun <genwocaigudian@gmail.com>
2020-09-16 20:19:34 +0800

    wechat

--- app/admin/controller/Wechat.php
+++ app/admin/controller/Wechat.php
@@ -2,17 +2,123 @@
 namespace app\admin\controller;
 
 use app\common\lib\Show;
-use app\common\services\Wechat as WechatService;
 use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use think\App;
 
 class Wechat extends AdminAuthBase
 {
+    protected $config;
+    public function __construct()
+    {
+        $this->config = config('wx');
+    }
+
     public function menu()
     {
-        $accessToken = WechatService::getAccessToken();
+        $url = sprintf($this->config['get_at_url'], config('wx.app_id'), config('wx.app_secret'));
         $client = new Client();
-        $response = $client->get('https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token='.$accessToken);
-        $menu = json_decode($response->getBody(), true);
-        return Show::success($menu);
+        $response = $client->get($url);
+        $at = json_decode($response->getBody(), true);
+        $accessToken = $at['access_token'];
+
+        $menuUrl = sprintf($this->config['get_custom_menu'], $accessToken);
+        $menuRes = $client->get($menuUrl);
+        $res = json_decode($menuRes->getBody(), true);
+
+        return Show::success($res);
+    }
+
+    public function save()
+    {
+        $json = '
+            {
+                "button": [
+                    {
+                        "name": "微1",
+                        "sub_button": [{
+                                "type": "view",
+                                "name": "微主页",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            },
+                            {
+                                "type": "view",
+                                "name": "2",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            },
+                            {
+                                "type": "view",
+                                "name": "3",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "微生活",
+                        "sub_button": [{
+                                "type": "view",
+                                "name": "1",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            },
+                            {
+                                "type": "view",
+                                "name": "2",
+                                "url": "https://gyjj.schoolpi.net/api/cate/index"
+                            },
+                            {
+                                "type": "view",
+                                "name": "3",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "微服务",
+                        "sub_button": [{
+                                "type": "view",
+                                "name": "1",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            },
+                            {
+                                "type": "view",
+                                "name": "2",
+                                "url": "https://gyjj.schoolpi.net/api/index/index"
+                            },
+                            {
+                                "type": "view",
+                                "name": "3",
+                                "url": "https://gyjj.schoolpi.net/api/cate/index"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ';
+
+        $url = sprintf($this->config['get_at_url'], config('wx.app_id'), config('wx.app_secret'));
+        $client = new Client();
+        $response = $client->get($url);
+        $at = json_decode($response->getBody(), true);
+        $accessToken = $at['access_token'];
+
+        $menuUrl = sprintf($this->config['create_menu'], $accessToken);
+//        $res = $client->post($menuUrl, [ RequestOptions::JSON => ['json' => $json]]);
+        $res = curl_post_json($menuUrl, $json);
+        return Show::success();
+    }
+
+    public function list()
+    {
+        $url = sprintf($this->config['get_at_url'], config('wx.app_id'), config('wx.app_secret'));
+        $client = new Client();
+        $response = $client->get($url);
+        $at = json_decode($response->getBody(), true);
+        $accessToken = $at['access_token'];
+
+        $userListUrl = sprintf($this->config['user_list'], $accessToken, '');
+        $userList = $client->get($userListUrl);
+        $res = json_decode($userList->getBody(), true);
+
+        return Show::success($res);
     }
 }
--- config/wx.php
+++ config/wx.php
@@ -11,9 +11,26 @@ return [
     'get_code_url' => "https://open.weixin.qq.com/connect/oauth2/authorize?" .
             "appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect",
     'redirect_uri' => '/api/token/code',
+
 	//获取token
     'get_token_url' => "https://api.weixin.qq.com/sns/oauth2/access_token?" .
             "appid=%s&secret=%s&code=%s&grant_type=authorization_code",
+
+    //获取access_token
+    'get_at_url' => "https://api.weixin.qq.com/cgi-bin/token?" .
+        "grant_type=client_credential&appid=%s&secret=%s",
+
+    //获取自定义菜单
+    'get_custom_menu' => "https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?" .
+        "access_token=%s",
+
+    //创建自定义菜单
+    'create_menu' => "https://api.weixin.qq.com/cgi-bin/menu/create?" .
+        "access_token=%s",
+
+    //获取微信关注用户列表
+    'user_list' => "https://api.weixin.qq.com/cgi-bin/user/get?" .
+        "access_token=%s&next_openid=%s",
 
     'api_token_pre' => 'api_token_',
 	//随机秘钥
