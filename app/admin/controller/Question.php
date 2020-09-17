9dbbc0eb75849be9a81c5bfbc454bb0b39dfbaa6
likun <genwocaigudian@gmail.com>
2020-09-17 18:05:24 +0800

    question

---
+++ app/admin/controller/Question.php
@@ -0,0 +1,222 @@
+<?php
+namespace app\admin\controller;
+
+use app\admin\validate\Question as QuestionValidate;
+use app\common\lib\Excel as ExcelLib;
+use app\common\lib\Show;
+use app\common\services\Question as QuestionService;
+use think\facade\Log;
+use think\response\Json;
+
+class Question extends AdminAuthBase
+{
+    /**
+     * @return Json
+     * @throws \think\db\exception\DbException
+     */
+    public function index()
+    {
+        $data = [];
+        $list = (new QuestionService())->getPaginateList($data, 10);
+        
+        return Show::success($list);
+    }
+
+    /**
+     * 新增
+     * @return Json
+     */
+    public function save()
+    {
+        if (!$this->request->isPost()) {
+            return Show::error('非法请求');
+        }
+        $data = input('post.');
+
+        $validate = new QuestionValidate();
+        if (!$validate->scene('save')->check($data)) {
+            return Show::error($validate->getError());
+        }
+
+        $data['user_id'] = $this->userId;
+        $data['start_time'] = $data['start_time'] . '00:00:00';
+        $data['end_time'] = $data['end_time'] . '23:59:59';
+
+        try {
+            $result = (new QuestionService())->insertData($data);
+        } catch (\Exception $e) {
+            Log::error('admin/question/save 错误:' . $e->getMessage());
+            return Show::error($e->getMessage(), $e->getCode());
+        }
+
+        return Show::success($result);
+    }
+
+    /**
+     * 详情
+     * @return Json
+     */
+    public function read()
+    {
+        $id = input('param.id', 0, 'intval');
+        try {
+            $result = (new QuestionService())->getNormalById($id);
+        } catch (\Exception $e) {
+            Log::error('admin/question/read 错误:' . $e->getMessage());
+            return Show::error($e->getMessage(), $e->getCode());
+        }
+
+        return Show::success($result);
+    }
+
+    /**
+     * 更新数据
+     * @return Json
+     */
+    public function update()
+    {
+        if (!$this->request->isPost()) {
+            return Show::error('非法请求');
+        }
+
+        $id = input('param.id', 0, 'intval');
+        $data = input('post.');
+//        $data = $this->request->only(['is_hot', 'is_top', 'title', 'content'], 'post');
+
+        try {
+            $res = (new QuestionService())->update($id, $data);
+        } catch (\Exception $e) {
+            return Show::error($e->getMessage());
+        }
+
+        return Show::success();
+    }
+
+    /**
+     * 删除数据
+     * @return Json
+     */
+    public function delete()
+    {
+        if (!$this->request->isPost()) {
+            return Show::error('非法请求');
+        }
+
+        $id = input("param.id");
+
+        try {
+            $res = (new QuestionService())->del($id);
+        } catch (\Exception $e) {
+            return Show::error($e->getMessage());
+        }
+
+        return Show::success();
+    }
+
+    /**
+     * @return Json
+     */
+    public function export()
+    {
+        $data = [];
+        $name = input('param.name', '', 'trim');
+        $number = input('param.number', '', 'trim');
+        $month = input('param.month', '', 'trim');
+        $id = input('param.id');
+        if (!empty($name)) {
+            $data['username'] = $name;
+        }
+        if (!empty($number)) {
+            $data['card_number'] = $number;
+        }
+        if (!empty($id)) {
+            $data['id'] = $id;
+        }
+        if (!empty($month)) {
+            $data['month'] = $month;
+        }
+
+        // 查询要导出的数据
+        $result = (new SalaryService())->getList($data);
+
+        if (!$result) {
+            return Show::error('没有数据可导出');
+        }
+
+        $data = [];
+
+        foreach ($result as $k => $v){
+            $data[$k]['username']=$v['username'];
+            $data[$k]['card_number']=$v['card_number'];
+            $data[$k]['department']=$v['department'];
+            $data[$k]['basic_salary']=$v['basic_salary'];
+            $data[$k]['post_salary']=$v['post_salary'];
+            $data[$k]['attendance_deduction']=$v['attendance_deduction'];
+            $data[$k]['other_deduction']=$v['other_deduction'];
+            $data[$k]['social_security']=$v['social_security'];
+            $data[$k]['provident_fund']=$v['provident_fund'];
+            $data[$k]['taxes']=$v['taxes'];
+            $data[$k]['paid_salary']=$v['paid_salary'];
+            $data[$k]['month']=$v['month'];
+        }
+//        $filename = "报修数据文档".date('YmdHis');
+        $filename = "薪酬数据文档";
+        $header = [
+            ['column' => 'username', 'name' => '姓名', 'width' => 15],
+            ['column' => 'month', 'name' => '月份', 'width' => 15],
+            ['column' => 'card_number', 'name' => '老师编号', 'width' => 15],
+            ['column' => 'department', 'name' => '部门', 'width' => 15],
+            ['column' => 'basic_salary', 'name' => '基本工资', 'width' => 15],
+            ['column' => 'post_salary', 'name' => '岗位工资', 'width' => 15],
+            ['column' => 'attendance_deduction', 'name' => '考勤扣款', 'width' => 15],
+            ['column' => 'other_deduction', 'name' => '其他扣款', 'width' => 15],
+            ['column' => 'social_security', 'name' => '社保', 'width' => 15],
+            ['column' => 'provident_fund', 'name' => '公积金', 'width' => 15],
+            ['column' => 'taxes', 'name' => '个税', 'width' => 15],
+            ['column' => 'paid_salary', 'name' => '应发工资', 'width' => 15],
+        ];
+        $download_url=(new ExcelLib())->exportSheelExcel($data,$header,$filename);//获取下载链接
+
+        if($download_url){
+            return Show::success(['url' => $download_url]);
+        }
+
+        return Show::error();
+    }
+
+    public function import()
+    {
+        $excel = new ExcelLib();
+        $data = $excel->importExcel();
+
+        if (empty($data)) {
+            return Show::error('导入内容为空');
+        }
+
+        $insertData = [];
+        foreach ($data as $datum) {
+            $temp = [
+                'username' => $datum['A'],
+                'month' => $datum['B'],
+                'card_number' => $datum['C'],
+                'department' => $datum['D'],
+                'basic_salary' => $datum['E'],
+                'post_salary' => $datum['F'],
+                'attendance_deduction' => $datum['G'],
+                'other_deduction' => $datum['H'],
+                'social_security' => $datum['I'],
+                'provident_fund' => $datum['J'],
+                'taxes' => $datum['K'],
+                'paid_salary' => $datum['L'],
+                'status' => config('status.mysql.table_normal'),
+            ];
+            array_push($insertData, $temp);
+        }
+
+        $res = (new SalaryService())->addAll($insertData);
+        if (!$res) {
+            return Show::error('插入失败');
+        }
+        return Show::success();
+    }
+}
---
+++ app/common/model/QuestionProblem.php
@@ -0,0 +1,165 @@
+<?php
+
+
+namespace app\common\model;
+
+use think\Collection;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\DbException;
+use think\db\exception\ModelNotFoundException;
+use think\model\concern\SoftDelete;
+
+class QuestionProblem extends BaseModel
+{
+    use SoftDelete;
+    protected $deleteTime = 'delete_time';
+
+    protected $type = [
+        'start_time'  =>  'timestamp',
+        'end_time'  =>  'timestamp'
+    ];
+
+    protected $hidden = [
+        'create_time',
+        'update_time',
+        'delete_time'
+    ];
+
+    /**
+     * @param $id
+     * @return array|bool|\think\Model|null
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    public function getById($id)
+    {
+        $id = intval($id);
+        if (!$id) {
+            return false;
+        }
+        $res = $this->find($id);
+        return $res;
+    }
+
+    /**
+     * 根据title查询数据
+     * @param $title
+     * @return array|bool|\think\Model|null
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function getNewsByTitle($title)
+    {
+        if (empty($title)) {
+            return false;
+        }
+
+        $where = [
+            'title' => $title,
+            'status' => config('status.mysql.table_normal'),
+        ];
+
+        return $this->where($where)->find();
+    }
+
+    /**
+     * @param string $field
+     * @return Collection
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    public function getNormalList($field = "*")
+    {
+        $where = [
+            "status" => config("status.mysql.table_normal"),
+        ];
+
+        $query = $this->newQuery();
+        $result = $query->where($where)
+            ->field($field)
+//            ->order($order)
+            ->select();
+        //echo $this->getLastSql();exit;
+        return $result;
+    }
+
+    /**
+     * @param $likeKeys
+     * @param $data
+     * @param string $field
+     * @return Collection
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    public function getList($likeKeys, $data, $field = "*")
+    {
+        $res = $this->newQuery();
+        if (!empty($likeKeys)) {
+            $res = $res->withSearch($likeKeys, $data);
+        }
+
+        $result = $res->field($field)->select();
+//        echo $res->getLastSql();exit;
+        return $result;
+    }
+
+    /**
+     * title查询条件表达式
+     * 调用withSearch方法时触发
+     * @param $query
+     * @param $value
+     */
+    public function searchTitleAttr($query, $value)
+    {
+        $query->where('title', 'like', '%' . $value . '%');
+    }
+
+    /**
+     * @param $id
+     * @param string $field
+     * @param int $num
+     * @return Collection
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    public function getPaginateList($id, $field = "*", $num = 10)
+    {
+        $order = [
+            "sequence" => "asc",
+            "id" => "asc",
+        ];
+        $result = $this->where(['question_id' => $id])->field($field)->order($order)->paginate($num);
+        //echo $this->getLastSql();exit;
+        return $result;
+    }
+
+    /**
+     * 根据主键ID更新数据表中的数据
+     * @param $id
+     * @param $data
+     * @return bool
+     */
+    public function deleteById($id, $data)
+    {
+        if (empty($id) || empty($data) || !is_array($data)) {
+            return false;
+        }
+
+        $where = [];
+
+        if (is_array($id)) {
+            $where[] = ['id', 'in', $id];
+        } else {
+            $where[] = ['id', '=', intval($id)];
+        }
+
+        $res = $this->where($where)->save($data);
+        //echo $this->getLastSql();exit;
+        return $res;
+    }
+}
