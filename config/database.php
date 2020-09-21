f276ab3401fc6567c624426d38b441c407ce992a
likun <genwocaigudian@gmail.com>
2020-09-21 15:20:51 +0800

    oracle

--- config/database.php
+++ config/database.php
@@ -62,15 +62,15 @@ return [
             // 数据库类型
             'type'            => '\misuoka\think\Oracle',
             // 服务器地址
-            'hostname'        => '172.16.9.20', // 填写数据库 IP 地址
+            'hostname'        => env('oracle.hostname', '127.0.0.1'), // 填写数据库 IP 地址
             // 数据库名
             'database'        => '', // 数据库实例 SID 名称，如 ORCL
             // 用户名
-            'username'        => 'jwzjk', // 用户名
+            'username'        => env('oracle.username', ''), // 用户名
             // 密码
-            'password'        => 'Hfgyxx_pai', // 密码
+            'password'        => env('oracle.password', ''), // 密码
             // 端口
-            'hostport'        => '1521', // 端口号，如 1521
+            'hostport'        => env('oracle.hostport', ''), // 端口号，如 1521
             // 数据库连接参数
             'params'          => [],
             // 数据库编码默认采用utf8
