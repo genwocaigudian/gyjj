88d3684778722ed28dce8af0e3a02cb4904afa9f
likun <genwocaigudian@gmail.com>
2020-09-28 14:39:00 +0800

    schedule

--- app/controller/Test1.php
+++ app/controller/Test1.php
@@ -3,7 +3,9 @@ namespace app\controller;
 
 use app\BaseController;
 use app\common\lib\Show;
+use app\common\model\Jskcb;
 use app\common\model\Jsxxb;
+use app\common\model\Xskcb;
 use app\common\model\Xsxxb;
 
 class Test1 extends BaseController
@@ -11,10 +13,10 @@ class Test1 extends BaseController
     public function index()
     {
         $xh = '0304150226';
-        $res = (new Xsxxb())->getByXH($xh);
+        $res = (new Xskcb())->getListByXH($xh);
 
         $zgh = '01032';
-        $res = (new Jsxxb())->getByZGH($zgh);
+        $res = (new Jskcb())->getListByZGH($zgh);
         return Show::success($res);
     }
 }
