926725464296425df9c9b6cdf702fed5188edd5c
likun <genwocaigudian@gmail.com>
2020-09-29 16:05:54 +0800

    selection

--- app/api/controller/Selection.php
+++ app/api/controller/Selection.php
@@ -4,8 +4,11 @@ namespace app\api\controller;
 
 use app\common\lib\Arr;
 use app\common\lib\Show;
+use app\common\services\Selection as SelectionService;
 use app\common\services\Selection as SelectionServices;
 use app\api\validate\Sresult as SelectionValidate;
+use app\common\services\SelectionOption;
+use think\facade\Log;
 use think\response\Json;
 
 class Selection extends AuthBase
@@ -26,27 +29,18 @@ class Selection extends AuthBase
     }
 
     /**
-     * 新增
+     * 详情
      * @return Json
      */
-    public function save()
+    public function read()
     {
-        if (!$this->request->isPost()) {
-            return Show::error('非法请求');
-        }
-        $data = input('post.');
-        
-        $validate = new SelectionValidate();
-        if (!$validate->scene('save')->check($data)) {
-            return Show::error($validate->getError());
-        }
-
+        $id = input('param.id', 0, 'intval');
         try {
-            $result = (new SelectionServices())->insertData($data);
+            $result = (new SelectionOption())->getPaginateList($id);
         } catch (\Exception $e) {
             return Show::error($e->getMessage());
         }
-        
+
         return Show::success($result);
     }
 }
