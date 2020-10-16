92b399ccd772e8e2951f1065c5c8d2071ac020e7
likun <genwocaigudian@gmail.com>
2020-10-16 16:43:25 +0800

    repair

--- app/api/validate/Repair.php
+++ app/api/validate/Repair.php
@@ -7,16 +7,14 @@ use think\Validate;
 class Repair extends Validate
 {
     protected $rule = [
-        'title' => 'require',
         'repair_cate_id' => 'require',
     ];
     
     protected $message = [
-        'title' => 'title不可为空',
         'repair_cate_id' => 'repair_cate_id不可为空',
     ];
     
     protected $scene = [
-        'save' => ['title', 'repair_cate_id'],
+        'save' => ['repair_cate_id'],
     ];
 }
