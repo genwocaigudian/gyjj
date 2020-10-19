907fd8581ba611030667529a75641bdab0177ea3
likun <genwocaigudian@gmail.com>
2020-10-19 16:28:47 +0800

    sync news

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -13,14 +13,21 @@ class Index extends BaseController
 
     public function test()
     {
-//        $xmlStr = file_get_contents('http://www.hfgyxx.com/rss/news_10601_1060108.xml');
-//        $xml = new \SimpleXMLElement($xmlStr);
-//        $res = $xml->xpath();
-
-        $values = simplexml_load_file('http://www.hfgyxx.com/rss/news_10601_1060108.xml');
-
-        foreach ($values as $value) {
-            var_dump($value->item);die();
+        $xmlStr = file_get_contents('http://www.hfgyxx.com/rss/news_10601_1060108.xml');
+        $obj = simplexml_load_string($xmlStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $eJSON = json_encode($obj);
+        $dJSON = json_decode($eJSON, true);
+        foreach ($dJSON['channel']['item'] as $value) {
+            $data = [
+                'title' => $value['title'],
+                'desc' => $value['description'],
+                'cate_id' => 2,
+                'xwbh' => $value['xwbh'],
+                'img_urls' => json_encode((array)$value['enclosure']['@attributes']['url']),
+                'pub_date' => $value['pubDate'],
+                'user_id' => 1,
+            ];
+            halt($data);
         }
         return Show::success([]);
     }
