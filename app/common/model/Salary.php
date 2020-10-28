dcd8890b7636e2661f6b4582a459b4f048fba921
likun <genwocaigudian@gmail.com>
2020-10-28 17:39:03 +0800

    salary

--- app/common/model/Salary.php
+++ app/common/model/Salary.php
@@ -119,12 +119,15 @@ class Salary extends BaseModel
      */
     public function getPaginateList($likeKeys, $data, $field = "*", $num = 10)
     {
+        $order = [
+            'id' => 'desc'
+        ];
         if (!empty($likeKeys)) {
             $res = $this->withSearch($likeKeys, $data);
         } else {
             $res = $this;
         }
-        $result = $res->field($field)->paginate($num);
+        $result = $res->field($field)->order($order)->paginate($num);
 //        echo $this->getLastSql();exit;
         return $result;
     }
