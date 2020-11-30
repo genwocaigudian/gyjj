152ff5b1f39af44354e64b25677b81f93c438c44
likun <genwocaigudian@gmail.com>
2020-11-30 10:36:41 +0800

    schedule

--- app/api/controller/Schedule.php
+++ app/api/controller/Schedule.php
@@ -50,6 +50,40 @@ class Schedule extends AuthBase
     /**
      * @return Json
      */
+    public function index1()
+    {
+        $data = input('param.');
+        $list = [];
+
+        $validate = new ScheduleValidate();
+        if (!$validate->scene('index')->check($data)) {
+            return Show::error($validate->getError());
+        }
+
+        $xq = $data['xq'];
+        $xn = $data['xn'];
+        switch ($this->type) {
+            case 2:
+                $model = new XskcbModel();
+                break;
+            default:
+                $model = new JskcbModel();
+                break;
+        }
+        try {
+            $user = (new \app\common\services\User())->getNormalUserById($this->userId);
+            $list = $model->getList($user['number'], $xn, $xq);
+        } catch (\Exception $e) {
+            return Show::success($list);
+        }
+
+        $list = Arr::groupArr($list->toArray(), 'XQJ');
+        return Show::success($list);
+    }
+
+    /**
+     * @return Json
+     */
     public function group()
     {
         switch ($this->type) {
