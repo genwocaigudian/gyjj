c01746accf2ab985ef218094a3bf4b8be3a25b69
likun <genwocaigudian@gmail.com>
2020-11-30 11:34:21 +0800

    schedule

--- app/api/controller/Schedule.php
+++ app/api/controller/Schedule.php
@@ -78,6 +78,9 @@ class Schedule extends AuthBase
         }
 
         $list = Arr::groupArr($list->toArray(), 'XQJ');
+        foreach ($list as $key => &$value) {
+            $value = Arr::arraySort($value, SORT_ASC);
+        }
         return Show::success($list);
     }
 
