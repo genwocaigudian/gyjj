e7bd040d3d63835750fe237a66926c4f75b3fbb9
likun <genwocaigudian@gmail.com>
2020-11-30 11:25:26 +0800

    schedule

--- app/common/lib/Arr.php
+++ app/common/lib/Arr.php
@@ -86,6 +86,22 @@ class Arr
         return $grouped;
     }
 
+    /**
+     * 二维数组根据某个字段排序
+     * @param array $array 要排序的数组
+     * @param string $keys   要排序的键字段
+     * @param string $sort  排序类型  SORT_ASC     SORT_DESC
+     * @return array 排序后的数组
+     */
+    public function arraySort($array, $keys, $sort = SORT_DESC) {
+        $keysValue = [];
+        foreach ($array as $k => $v) {
+            $keysValue[$k] = $v[$keys];
+        }
+        array_multisort($keysValue, $sort, $array);
+        return $array;
+    }
+
     public static function uniqueByKey($arr, $key){
         $tmp_arr = array();
         foreach($arr as $k => $v)
